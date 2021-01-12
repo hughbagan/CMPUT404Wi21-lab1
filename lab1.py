47669e52c9af22459819775143941c45922974e7
hughbagan <25750667+hughbagan@users.noreply.github.com>
2021-01-11 18:17:36 -0700

    Completed Question 8

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import requests
-print(requests.__version__)
+print("requests version:", requests.__version__)
 # https://requests.readthedocs.io/en/master/
-print(requests.get('http://www.google.com'))
-
+print("GET http://www.google.com:", requests.get('http://www.google.com'))
+print(requests.get('https://raw.githubusercontent.com/hughbagan/CMPUT404Wi21-lab1/master/lab1.py').text)
